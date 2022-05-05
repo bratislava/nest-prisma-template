c644218a64d2e8aad7342bf286debb936c7e0a2c
Richi <richard.dvorsky@onlim.com>
2022-05-05 16:06:10 +0200

    fixed lot of errors also with harbor

--- bin/deploy.ts
+++ bin/deploy.ts
@@ -22,10 +22,15 @@ function image_tag(options) {
 
 function tag(options) {
   let untracked = '';
+  let branch = '-' + options.branch;
   if (options.untracked) {
-    untracked = 'untracked-';
+    untracked = '-untracked';
   }
-  return `local-${untracked}${options.commit}`;
+  if (options.branch === 'origin/master') {
+    branch = '';
+  }
+
+  return `bratiska-cli-${options.commit}${branch}${untracked}`;
 }
 
 function image(options) {
@@ -34,6 +39,10 @@ function image(options) {
 
 function manifest(options) {
   return `manifest-${tag(options)}.yaml`;
+}
+
+function pull_secret_name(options: any): string {
+  return `harbor-secret-${options.env}-${options.namespace}-bratiska-cli`;
 }
 
 function finished(): void {
@@ -63,7 +72,9 @@ function assign_env_vars(options: any) {
   process.env['HOSTNAME'] = options.host;
   process.env['IMAGE'] = image(options);
   process.env['TAG'] = tag(options);
+  process.env['COMMIT'] = options.commit;
   process.env['NAMESPACE'] = options.namespace;
+  process.env['IMAGE_PULL_SECRET'] = pull_secret_name(options);
 }
 
 function get_repository_url(): Bash {
@@ -153,6 +164,23 @@ function get_pods(): Bash {
   return { res: result.stdout.trim(), err: result.stderr };
 }
 
+function get_pull_secret(options: any): Bash {
+  const result = cp.spawnSync(
+    'kubectl',
+    [
+      'get',
+      'secret',
+      pull_secret_name(options),
+      ` --namespace=${options.namespace}`,
+      '--output=yaml',
+    ],
+    {
+      encoding: 'utf8',
+    },
+  );
+  return { res: result.stdout.trim(), err: result.stderr };
+}
+
 function get_docker(): Bash {
   const result = cp.spawnSync('docker', ['-v'], {
     encoding: 'utf8',
@@ -210,6 +238,7 @@ function build_kustomize_manifest(options: any) {
 }
 
 function apply_to_kubernetes(manifest_path: string) {
+  log(chalk.reset(''));
   cp.spawnSync('kubectl', ['apply', `-f=${manifest_path}`], {
     stdio: 'inherit',
   });
@@ -224,12 +253,12 @@ try {
   program
     .version('1.0')
     .description(
-      'Simple Bratiska-cli utility for deploying and managing Bratislava Inovation apps',
+      'Simple Bratiska-cli utility for deploying and managing Bratislava Innovation apps',
     )
     .option('-n, --namespace <namespace>', 'Namespace', 'standalone')
     .option('-d, --deployment <deployment>', 'Deployment app')
     .option('-h, --host <host>', 'Host url address')
-    .option('-e, --env <env>', 'Deployment enviroment')
+    .option('-e, --env <env>', 'Deployment environment')
     .option(
       '-r, --registry <url>',
       'Docker image registry url',
@@ -252,7 +281,7 @@ try {
     options.version = pack.version;
   }
 
-  line('(0) Checking git...');
+  line('(1) Checking git...');
   const pwd_bash = get_pwd();
   if (pwd_bash.err !== '') {
     throw new Error('There was an issue getting current working directory!');
@@ -303,12 +332,12 @@ try {
     options.branch,
   );
   options.merged = false;
-  if (remote_commit_bash.err !== '') {
+  if (remote_commit_bash.err === '') {
     options.merged = true;
   }
   ok();
 
-  line('(1) Checking current kubernetes cluster...');
+  line('(2) Checking current kubernetes cluster...');
   const response_cluster = get_cluster();
   options.cluster = response_cluster.res;
   if (response_cluster.err !== '') {
@@ -319,7 +348,7 @@ try {
   }
   ok();
 
-  line('(2) Checking kubernetes connection to cluster...');
+  line('(3) Checking kubernetes connection to cluster...');
   const pods = get_pods();
   if (pods.err !== '') {
     throw new Error(
@@ -328,7 +357,7 @@ try {
   }
   ok();
 
-  line('(3) Checking chosen kubernetes cluster with enviroment...');
+  line('(4) Checking chosen kubernetes cluster with environment...');
   if (typeof options.env === 'undefined') {
     options.env = map_cluster_to_env(options.cluster);
   } else if (options.env != map_cluster_to_env(options.cluster)) {
@@ -370,19 +399,38 @@ try {
       }
       break;
   }
-
   line(` we will use ${options.cluster}...`);
   ok();
 
+  line('(5) Determining host...');
   if (typeof options.host === 'undefined') {
     let env = options.env;
     if (options.env === 'prod') {
       env = '';
     }
     options.host = options.deployment + '.' + env + '.bratislava.sk';
+    line(` using this host '${options.host}'...`);
   }
+  ok();
 
-  line('(4) Checking docker...');
+  line(
+    `(6) Checking kubernetes harbor pull secret '${pull_secret_name(
+      options,
+    )}' ...`,
+  );
+  const pull_secret_bash = get_pull_secret(options);
+  if (pull_secret_bash.res === '') {
+    throw new Error(
+      `We have no harbor pull secrets with name '${pull_secret_name(
+        options,
+      )}'  on kubernetes cluster '${
+        options.cluster
+      }'. Please contact kubernetes admin to create pull secret.`,
+    );
+  }
+  ok();
+
+  line('(7) Checking docker...');
   const docker = get_docker();
   if (docker.err !== '') {
     throw new Error(
@@ -391,12 +439,12 @@ try {
   }
   ok();
 
-  message('(5) Building docker image...');
+  message('(8) Building docker image...');
   message(`Docker image tag: ${image_tag(options)}`);
   docker_build(options);
   finished();
 
-  line('(6) Checking if image was created...');
+  line('(9) Checking if image was created...');
   const image = check_docker_image(options);
   if (image.err !== '') {
     throw new Error(
@@ -414,11 +462,11 @@ try {
   }
   ok();
 
-  message('(7) Pushing image to regitry ...');
+  message('(10) Pushing image to regitry ...');
   push_docker_image(options);
   finished();
 
-  line('(8) Checking if image was pushed...');
+  line('(11) Checking if image was pushed...');
   const image_r = check_image_in_registry(options);
   if (image_r.err !== '') {
     throw new Error(
@@ -427,25 +475,26 @@ try {
   }
   ok();
 
-  line('(9) Creating env variables for kustomize...');
+  line('(12) Creating env variables for kustomize...');
   assign_env_vars(options);
   ok();
 
-  line('(10) Building kustomize manifest...');
+  line('(13) Building kustomize manifest...');
   build_kustomize_manifest(options);
   ok();
 
-  line('(11) Checking kustomize manifest...');
+  line('(14) Checking kustomize manifest...');
   const manifest_path = `${options.pwd}/${manifest(options)}`;
   if (!fs.existsSync(manifest_path)) {
     throw new Error(`We had an error creating kustomize manifest.`);
   }
   ok();
 
-  line('(12) Deploying to kubernetes...');
+  line('(15) Deploying to kubernetes...');
   apply_to_kubernetes(manifest_path);
   finished();
 
+  /*
   line('(13) Cleaning manifest...');
   try {
     fs.unlinkSync(manifest_path);
@@ -453,6 +502,8 @@ try {
     throw new Error(`We had an error by cleaning manifest file`);
   }
   ok();
+
+  */
 } catch (e: any) {
   log('');
   log('\x1b[31m', e.message);
