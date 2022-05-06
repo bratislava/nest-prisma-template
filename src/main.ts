64e72b8429614835ba2ca3bfc38925d2554d6d81
Richi <richard.dvorsky@onlim.com>
2022-05-06 17:30:14 +0200

    fixed cutomize and ports

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Nest prisma template')
